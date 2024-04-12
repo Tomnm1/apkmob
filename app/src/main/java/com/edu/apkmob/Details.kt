a80b97c37dd026ae9a3c52ce2dbe5c07b6b848b1
Tomnm1 <tomnm77@gmail.com>
2024-04-12 13:49:05 +0200

    3

--- app/src/main/java/com/edu/apkmob/Details.kt
+++ app/src/main/java/com/edu/apkmob/Details.kt
@@ -5,13 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material.icons.outlined.Refresh
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class Details() : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +38,6 @@ class Details() : ComponentActivity() {
 
 @Composable
 fun DetailsLayout(trail: Trail) {
-    val message = "To jest nowa aktywność!"
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -33,6 +45,44 @@ fun DetailsLayout(trail: Trail) {
         ) {
             Text(text = trail.name, modifier = Modifier.padding(16.dp))
             Text(text = trail.description, modifier = Modifier.padding(16.dp))
+            Text(
+                text = "Etapy:",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    shadow = Shadow(
+                    color = Color.Blue, offset = Offset(5.0f, 10.0f),
+                    blurRadius = 3f,
+                ))
+            )
+            trail.stages.forEachIndexed{index, stage ->
+                //odpowiadające czasy dodać potem /indeks +1
+                Text(text = "${index+1} ${stage}", modifier = Modifier.padding(16.dp))
+            }
+            Text("", modifier = Modifier.padding(32.dp))
+
+            Row (horizontalArrangement = Arrangement.Center, ){
+                IconButton(onClick = { /* do something */ }) {
+                    Icon(Icons.Outlined.Refresh,
+                        contentDescription = "Refresh",
+                        tint = Color.Magenta,
+                        modifier = Modifier.size(48.dp))
+                }
+                IconButton(onClick = { /* do something */ }) {
+                    Icon(
+                        Icons.Outlined.PlayArrow,
+                        contentDescription = "Start",
+                        tint = Color.Magenta,
+                        modifier = Modifier.size(48.dp))
+                }
+                IconButton(onClick = { /* do something */ }) {
+                    Icon(
+                        Icons.Outlined.Close,
+                        contentDescription = "Stop",
+                        tint = Color.Magenta,
+                        modifier = Modifier.size(48.dp))
+                }
+
+            }
 
         }
 }
